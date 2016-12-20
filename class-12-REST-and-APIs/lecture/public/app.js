f07d2bc1ac32da8593041d7630b8416c805c6de1
Scott Schmidt <sjschmidt44@gmail.com>
2016-12-19 17:40:04 -0800

    remove lecture examples for class demo

--- class-12-REST-and-APIs/lecture/public/app.js
+++
@@ -1,7 +0,0 @@
-$.ajax({
-  url: 'https://api.github.com/user/repos?type=owner&sort=created',
-  type: 'GET',
-  headers: {
-    Authorization: `token ${githubToken}`
-  }
-}).done(data => console.log(data))
